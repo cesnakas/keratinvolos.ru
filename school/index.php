09ebf8033790417e505db9dc540c215df3c67d55
Sergei ★ Česnakas <10352420+cesnakas@users.noreply.github.com>
2021-10-09 16:21:13 +0400

    Update dir school

--- school/index.php
+++ school/index.php
@@ -3,7 +3,102 @@ require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("Обучение");
 ?>
 
-    Text here....
+    <!--<div class="section section_page_title">
+        <div class="container">
+            <p class="page_title">
+                ОБУЧЕНИЕ
+            </p>
+            <div class="breadcrumbs">
+                <ul>
+                    <li>
+                        <a href="index.php">
+                            ГЛАВНАЯ
+                        </a>
+                    </li>
+                    <li>
+							<span>
+								ОБУЧЕНИЕ
+							</span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>-->
+
+    <div class="section section_school">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-7 col-lg-7 col-md-12">
+                    <p class="school_title">
+                        Расписание семинаров
+                    </p>
+                    <p class="school_subtitle">
+                        <i class="fas fa-bookmark"></i>
+                        <span>Салонный уход дома от Global Keratin.</span>
+                    </p>
+                    <p class="school_descr">
+                        <i class="fas fa-calendar-alt"></i>Дата: 6 Сентября
+                    </p>
+                    <p class="school_descr">
+                        <i class="far fa-clock"></i>Время: 11:00-15:00
+                    </p>
+                    <p class="school_descr">
+                        <i class="fas fa-location-arrow"></i>Место: м.Курская, Нижний Сусальный переулок 4/5, Москва
+                    </p>
+                    <p class="school_descr">
+                        <i class="fas fa-info-circle"></i>1. Кратакая история компании: Создание JUVEXIN, его особенности, сырье и принцип работы. Распространение продукта на мировом рынке, популярность и уникальность.
+                    </p>
+                    <p class="school_descr">
+                        2. Спецификация домашних продуктов по типу/ проблеме волос: Универсальные продукты; Продукты для окрашенных волос; Продукты для волос с проблемами （нарушение себорегуляции, себорея, работа сальных и потовых желез）;
+                    </p>
+                    <p class="school_descr">
+                        3. Особенности работы домашних уходов: Классификация продуктов; Сочетаемость продуктов; Методы применения;
+                    </p>
+                    <p class="school_descr">
+                        4. Обзор новинок компании: Веганские продукты их особенность
+                    </p>
+                    <p class="school_descr">
+                        5. Ответы на вопросы и рекомендации.
+                    </p>
+                    <p class="school_descr">
+                        <i class="fas fa-ruble-sign"></i>Стоимость участия: Бесплатно
+                    </p>
+                    <p class="school_subtitle">
+                        <i class="fas fa-bookmark"></i>
+                        <span>Три вида кератина: Resistant, The Best, Miami Bombshell</span>
+                        Обучающий семинар Кератиновое выпрямление и восстановление волос.
+                    </p>
+                    <p class="school_descr">
+                        <i class="fas fa-calendar-alt"></i>Дата: 25 Августа, 10 и 23 Сентября, 13 и 29 Октября, 10 и 25 ноября
+                    </p>
+                    <p class="school_descr">
+                        <i class="far fa-clock"></i>Время: 11:00-15:00
+                    </p>
+                    <p class="school_descr">
+                        <i class="fas fa-location-arrow"></i>Место: м.Курская, Нижний Сусальный переулок 4/5, Москва
+                    </p>
+                    <p class="school_descr">
+                        <i class="fas fa-info-circle"></i>Описание: Презентация 3 видов кератина бренда GKhair.
+                    </p>
+                    <p class="school_descr">
+                        1.Кератин RESISTANT - кератиновое выпрямление и восстановление волос.
+                    </p>
+                    <p class="school_descr">
+                        2.Кератин THE BEST - № 1 в мире по кератиновому выпрямлению, реконструкции и восстановлению волос. Презентация новой улучшенной формулы.
+                    </p>
+                    <p class="school_descr">
+                        3.Кератин MIAMI BOMBSHELL - выпрямление, восстановление и нейтрализация не желательной желтизны блондированных волос.
+                    </p>
+                    <p class="school_descr">
+                        <i class="fas fa-ruble-sign"></i>Стоимость участия: Бесплатно
+                    </p>
+                </div>
+                <div class="col-xl-5 col-lg-5 col-md-12">
+                    <img src="<?=SITE_TEMPLATE_PATH?>/img/articles_single_1.webp" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
 
 <?php
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php");
