0c763df98fb47d291d59624835853aca3674f29d
Sergei ★ Česnakas <10352420+cesnakas@users.noreply.github.com>
2021-10-09 15:56:22 +0400

    Update dir gallery

--- gallery/detail.php
+++ gallery/detail.php
@@ -1,9 +1,156 @@
 <?php
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
-$APPLICATION->SetTitle("Новая страница");
+$APPLICATION->SetTitle("Галерея");
 ?>
 
-    Text here....
+    <!--<div class="section section_page_title">
+        <div class="container">
+            <p class="page_title">
+                ГАЛЕРЕЯ
+            </p>
+            <div class="breadcrumbs">
+                <ul>
+                    <li>
+                        <a href="index.php">
+                            ГЛАВНАЯ
+                        </a>
+                    </li>
+                    <li>
+                        <a href="index.php">
+                            ГАЛЕРЕЯ
+                        </a>
+                    </li>
+                    <li>
+							<span>
+								22.08.2021
+							</span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>-->
+
+    <div class="section section_gallery__details" >
+        <div class="container">
+            <p class="gallery_title">
+                все фото с шоу - мастер класса Валентины....
+            </p>
+            <div class="row">
+                <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-6">
+                    <div class="gallery">
+                        <a href="<?=SITE_TEMPLATE_PATH?>/img/product_4.png" data-fancybox="img_gal" data-width="1000" data-height="1000" class="image-gallery">
+                            <img data-src="<?=SITE_TEMPLATE_PATH?>/img/product_4.png" alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-6">
+                    <div class="gallery">
+                        <a href="<?=SITE_TEMPLATE_PATH?>/img/product_1.jpg" data-fancybox="img_gal" data-width="1000" data-height="1000" class="image-gallery">
+                            <img data-src="<?=SITE_TEMPLATE_PATH?>/img/product_1.jpg" alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-6">
+                    <div class="gallery">
+                        <a href="<?=SITE_TEMPLATE_PATH?>/img/product_2.jpg" data-fancybox="img_gal" data-width="1000" data-height="1000" class="image-gallery">
+                            <img data-src="<?=SITE_TEMPLATE_PATH?>/img/product_2.jpg" alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-6">
+                    <div class="gallery">
+                        <a href="<?=SITE_TEMPLATE_PATH?>/img/product_3.png" data-fancybox="img_gal" data-width="1000" data-height="1000" class="image-gallery">
+                            <img data-src="<?=SITE_TEMPLATE_PATH?>/img/product_3.png" alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-6">
+                    <div class="gallery">
+                        <a href="<?=SITE_TEMPLATE_PATH?>/img/product_4.png" data-fancybox="img_gal" data-width="1000" data-height="1000" class="image-gallery">
+                            <img data-src="<?=SITE_TEMPLATE_PATH?>/img/product_4.png" alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-6">
+                    <div class="gallery">
+                        <a href="<?=SITE_TEMPLATE_PATH?>/img/product_1.jpg" data-fancybox="img_gal" data-width="1000" data-height="1000" class="image-gallery">
+                            <img data-src="<?=SITE_TEMPLATE_PATH?>/img/product_1.jpg" alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-6">
+                    <div class="gallery">
+                        <a href="<?=SITE_TEMPLATE_PATH?>/img/product_2.jpg" data-fancybox="img_gal" data-width="1000" data-height="1000" class="image-gallery">
+                            <img data-src="<?=SITE_TEMPLATE_PATH?>/img/product_2.jpg" alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-6">
+                    <div class="gallery">
+                        <a href="<?=SITE_TEMPLATE_PATH?>/img/product_3.png" data-fancybox="img_gal" data-width="1000" data-height="1000" class="image-gallery">
+                            <img data-src="<?=SITE_TEMPLATE_PATH?>/img/product_3.png" alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-6">
+                    <div class="gallery">
+                        <a href="<?=SITE_TEMPLATE_PATH?>/img/product_4.png" data-fancybox="img_gal" data-width="1000" data-height="1000" class="image-gallery">
+                            <img data-src="<?=SITE_TEMPLATE_PATH?>/img/product_4.png" alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-6">
+                    <div class="gallery">
+                        <a href="<?=SITE_TEMPLATE_PATH?>/img/product_1.jpg" data-fancybox="img_gal" data-width="1000" data-height="1000" class="image-gallery">
+                            <img data-src="<?=SITE_TEMPLATE_PATH?>/img/product_1.jpg" alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-6">
+                    <div class="gallery">
+                        <a href="<?=SITE_TEMPLATE_PATH?>/img/product_2.jpg" data-fancybox="img_gal" data-width="1000" data-height="1000" class="image-gallery">
+                            <img data-src="<?=SITE_TEMPLATE_PATH?>/img/product_2.jpg" alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-6">
+                    <div class="gallery">
+                        <a href="<?=SITE_TEMPLATE_PATH?>/img/product_3.png" data-fancybox="img_gal" data-width="1000" data-height="1000" class="image-gallery">
+                            <img data-src="<?=SITE_TEMPLATE_PATH?>/img/product_3.png" alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-6">
+                    <div class="gallery">
+                        <a href="<?=SITE_TEMPLATE_PATH?>/img/product_4.png" data-fancybox="img_gal" data-width="1000" data-height="1000" class="image-gallery">
+                            <img data-src="<?=SITE_TEMPLATE_PATH?>/img/product_4.png" alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-6">
+                    <div class="gallery">
+                        <a href="<?=SITE_TEMPLATE_PATH?>/img/product_1.jpg" data-fancybox="img_gal" data-width="1000" data-height="1000" class="image-gallery">
+                            <img data-src="<?=SITE_TEMPLATE_PATH?>/img/product_1.jpg" alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-6">
+                    <div class="gallery">
+                        <a href="<?=SITE_TEMPLATE_PATH?>/img/product_2.jpg" data-fancybox="img_gal" data-width="1000" data-height="1000" class="image-gallery">
+                            <img data-src="<?=SITE_TEMPLATE_PATH?>/img/product_2.jpg" alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-6">
+                    <div class="gallery">
+                        <a href="<?=SITE_TEMPLATE_PATH?>/img/product_3.png" data-fancybox="img_gal" data-width="1000" data-height="1000" class="image-gallery">
+                            <img data-src="<?=SITE_TEMPLATE_PATH?>/img/product_3.png" alt="">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 <?php
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php");
--- gallery/index.php
+++ gallery/index.php
@@ -3,7 +3,177 @@ require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("Галерея");
 ?>
 
-    Text here....
+    <!--<div class="section section_page_title">
+        <div class="container">
+            <p class="page_title">
+                ГАЛЕРЕЯ
+            </p>
+            <div class="breadcrumbs">
+                <ul>
+                    <li>
+                        <a href="index.php">
+                            ГЛАВНАЯ
+                        </a>
+                    </li>
+                    <li>
+							<span>
+								ГАЛЕРЕЯ
+							</span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>-->
+
+    <div class="section section_gallery" >
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-6">
+                    <div class="gallery_item">
+                        <img data-src="<?=SITE_TEMPLATE_PATH?>/img/product_4.png" alt="">
+                        <div class="gallery_hover">
+                            <p class="gallery_hover__descr">
+                                Посмотреть все фото с шоу - мастер класса Валентины....
+                            </p>
+                            <a href="#" class="btn btn_yellow">
+                                СМОТРЕТЬ ВСЕ
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-6">
+                    <div class="gallery_item">
+                        <img data-src="<?=SITE_TEMPLATE_PATH?>/img/product_4.png" alt="">
+                        <div class="gallery_hover">
+                            <p class="gallery_hover__descr">
+                                Посмотреть все фото с шоу - мастер класса Валентины....
+                            </p>
+                            <a href="#" class="btn btn_yellow">
+                                СМОТРЕТЬ ВСЕ
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-6">
+                    <div class="gallery_item">
+                        <img data-src="<?=SITE_TEMPLATE_PATH?>/img/product_4.png" alt="">
+                        <div class="gallery_hover">
+                            <p class="gallery_hover__descr">
+                                Посмотреть все фото с шоу - мастер класса Валентины....
+                            </p>
+                            <a href="#" class="btn btn_yellow">
+                                СМОТРЕТЬ ВСЕ
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-6">
+                    <div class="gallery_item">
+                        <img data-src="<?=SITE_TEMPLATE_PATH?>/img/product_4.png" alt="">
+                        <div class="gallery_hover">
+                            <p class="gallery_hover__descr">
+                                Посмотреть все фото с шоу - мастер класса Валентины....
+                            </p>
+                            <a href="#" class="btn btn_yellow">
+                                СМОТРЕТЬ ВСЕ
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-6">
+                    <div class="gallery_item">
+                        <img data-src="<?=SITE_TEMPLATE_PATH?>/img/product_4.png" alt="">
+                        <div class="gallery_hover">
+                            <p class="gallery_hover__descr">
+                                Посмотреть все фото с шоу - мастер класса Валентины....
+                            </p>
+                            <a href="#" class="btn btn_yellow">
+                                СМОТРЕТЬ ВСЕ
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-6">
+                    <div class="gallery_item">
+                        <img data-src="<?=SITE_TEMPLATE_PATH?>/img/product_4.png" alt="">
+                        <div class="gallery_hover">
+                            <p class="gallery_hover__descr">
+                                Посмотреть все фото с шоу - мастер класса Валентины....
+                            </p>
+                            <a href="#" class="btn btn_yellow">
+                                СМОТРЕТЬ ВСЕ
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-6">
+                    <div class="gallery_item">
+                        <img data-src="<?=SITE_TEMPLATE_PATH?>/img/product_4.png" alt="">
+                        <div class="gallery_hover">
+                            <p class="gallery_hover__descr">
+                                Посмотреть все фото с шоу - мастер класса Валентины....
+                            </p>
+                            <a href="#" class="btn btn_yellow">
+                                СМОТРЕТЬ ВСЕ
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-6">
+                    <div class="gallery_item">
+                        <img data-src="<?=SITE_TEMPLATE_PATH?>/img/product_4.png" alt="">
+                        <div class="gallery_hover">
+                            <p class="gallery_hover__descr">
+                                Посмотреть все фото с шоу - мастер класса Валентины....
+                            </p>
+                            <a href="#" class="btn btn_yellow">
+                                СМОТРЕТЬ ВСЕ
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-6">
+                    <div class="gallery_item">
+                        <img data-src="<?=SITE_TEMPLATE_PATH?>/img/product_4.png" alt="">
+                        <div class="gallery_hover">
+                            <p class="gallery_hover__descr">
+                                Посмотреть все фото с шоу - мастер класса Валентины....
+                            </p>
+                            <a href="#" class="btn btn_yellow">
+                                СМОТРЕТЬ ВСЕ
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-6">
+                    <div class="gallery_item">
+                        <img data-src="<?=SITE_TEMPLATE_PATH?>/img/product_4.png" alt="">
+                        <div class="gallery_hover">
+                            <p class="gallery_hover__descr">
+                                Посмотреть все фото с шоу - мастер класса Валентины....
+                            </p>
+                            <a href="#" class="btn btn_yellow">
+                                СМОТРЕТЬ ВСЕ
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-6">
+                    <div class="gallery_item">
+                        <img data-src="<?=SITE_TEMPLATE_PATH?>/img/product_4.png" alt="">
+                        <div class="gallery_hover">
+                            <p class="gallery_hover__descr">
+                                Посмотреть все фото с шоу - мастер класса Валентины....
+                            </p>
+                            <a href="#" class="btn btn_yellow">
+                                СМОТРЕТЬ ВСЕ
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 <?php
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php");
